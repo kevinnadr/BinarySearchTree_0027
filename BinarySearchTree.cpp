7d1c192dcdfe8d5ec8d290ef5cde20ec0f37a014
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-24 10:22:33 +0700

    first commit

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,2 @@
+#include <iostream>              // Mengimpor library untuk input/output standar
+using namespace std;             // Menggunakan namespace std agar tidak perlu menulis std:: di depan fungsi/benda dari std

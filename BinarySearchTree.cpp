4f45788a76062108300f46ebad228e32e6368765
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-24 10:27:24 +0700

    untuk menyimpan data pada node dan memberikan pointer kanan dan kiri

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,3 +2,11 @@
 using namespace std;             // Menggunakan namespace std agar tidak perlu menulis std:: di depan fungsi/benda dari std
 
 class Node                      // Mendefinisikan kelas Node untuk struktur data pohon biner
+{
+public:
+    int info;                   // Menyimpan data/informasi pada node
+    Node* leftchild;            // Pointer ke anak kiri (subtree kiri)
+    Node* rightchild;           // Pointer ke anak kanan (subtree kanan)
+
+
+};

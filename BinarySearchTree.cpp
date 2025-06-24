28dd3706bfef3cc71419f164736888f8acec2e88
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-24 10:23:09 +0700

    Mendefinisikan kelas Node untuk struktur data pohon biner

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,2 +1,4 @@
 #include <iostream>              // Mengimpor library untuk input/output standar
 using namespace std;             // Menggunakan namespace std agar tidak perlu menulis std:: di depan fungsi/benda dari std
+
+class Node                      // Mendefinisikan kelas Node untuk struktur data pohon biner
